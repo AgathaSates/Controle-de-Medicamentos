106410c3c2ac5e0de7c85a09aad030a4ec1c44e8
Agatha Sates <satesagatha@gmail.com>
2025-06-05 10:25:38 -0300

    feat: montado base do projeto

---
+++ Controle-de-Medicamentos.ConsoleApp/Model/NotificationViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Controle_de_Medicamentos.ConsoleApp.Model;
+
+public class NotificationViewModel
+{
+    public string TitleAction { get; set; }
+    public string RegisterType { get; set; }
+    public string RegisterName { get; set; }
+    public string Action { get; set; }
+    public string Linkdestination { get; set; }
+
+
+    public NotificationViewModel(string titleAction, string registerType, string registerName, string action, string linkdestination)
+    {
+        TitleAction = titleAction;
+        RegisterType = registerType;
+        RegisterName = registerName;
+        Action = action;
+        Linkdestination = linkdestination;
+    }
+}
